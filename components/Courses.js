8ea3b06ca8c344925f88c183c54c9e87bff433ad
azmtbek <m.azamatbek@gmail.com>
2021-01-14 11:40:08 +0500

    final

--- components/Courses.js
+++ components/Courses.js
@@ -21,57 +21,79 @@ const Courses = () => {
         <div className={styles.listItem}>
           <a href="/courses/2">
             <img src="/courses.jpg" alt="" />{" "}
-            <div className={styles.listItem_title}>Web dasturlash</div>
+            <div className={styles.listItem_title}>Video Montaj</div>
             <div className={styles.listItem_tags}>
-              <span>JavaScript</span>
-              <span>React</span>
+              <span>After Effects</span>
+              <span>PremierPro</span>
             </div>
           </a>
         </div>{" "}
         <div className={styles.listItem}>
           <a href="/courses/1">
             <img src="/courses.jpg" alt="" />{" "}
-            <div className={styles.listItem_title}>Web dasturlash</div>
+            <div className={styles.listItem_title}>SMM va Kopirayting</div>
             <div className={styles.listItem_tags}>
-              <span>JavaScript</span>
-              <span>React</span> <span>JavaScript</span>
-              <span>React</span> <span>JavaScript</span>
-              <span>React</span>
+              <span>Facebook</span> <span>Youtube</span>
+              <span>Instagram</span> <span>Telegram</span>
+              <span>TikTok</span>
             </div>
           </a>
         </div>{" "}
         <div className={styles.listItem}>
           <a href="/courses/2">
             <img src="/courses.jpg" alt="" />{" "}
-            <div className={styles.listItem_title}>Web dasturlash</div>
+            <div className={styles.listItem_title}>Office Dasturlari</div>
             <div className={styles.listItem_tags}>
-              <span>JavaScript</span>
-              <span>React</span>
+              <span>MS Word</span>
+              <span>MS Excel</span>
+              <span>MS PowerPoint</span>
             </div>
           </a>
         </div>{" "}
         <div className={styles.listItem}>
           <a href="/courses/1">
             <img src="/courses.jpg" alt="" />{" "}
-            <div className={styles.listItem_title}>Web dasturlash</div>
+            <div className={styles.listItem_title}>Ingliz tili</div>
             <div className={styles.listItem_tags}>
-              <span>JavaScript</span>
-              <span>React</span> <span>JavaScript</span>
-              <span>React</span> <span>JavaScript</span>
-              <span>React</span>
+              <span>IELTS</span>
+              <span>Advanced</span> <span>Intermediate</span>
+              <span>Pre-Intermediate</span>
+              <span>Beginner</span>
             </div>
           </a>
         </div>{" "}
         <div className={styles.listItem}>
           <a href="/courses/2">
             <img src="/courses.jpg" alt="" />{" "}
-            <div className={styles.listItem_title}>Web dasturlash</div>
+            <div className={styles.listItem_title}>Grafik Dizayni</div>
             <div className={styles.listItem_tags}>
-              <span>JavaScript</span>
-              <span>React</span>
+              <span>Adobe Illustrator</span>
+              <span>Photoshop</span>
             </div>
           </a>
         </div>{" "}
+        <div className={styles.listItem}>
+          <a href="/courses/2">
+            <img src="/courses.jpg" alt="" />{" "}
+            <div className={styles.listItem_title}>Arxitektura</div>
+            <div className={styles.listItem_tags}>
+              <span>AutoCAD</span>
+              <span>2D</span>
+              <span>3D</span>
+            </div>
+          </a>
+        </div>{" "}
+        <div className={styles.listItem}>
+          <a href="/courses/2">
+            <img src="/courses.jpg" alt="" />{" "}
+            <div className={styles.listItem_title}>Grafik Dizayni</div>
+            <div className={styles.listItem_tags}>
+              <span>Adobe Illustrator</span>
+              <span>Photoshop</span>
+            </div>
+          </a>
+        </div>{" "}
+        <div className={styles.listItem}></div>
       </div>{" "}
       <Button link="courses" text="Batafsil" />
     </div>
