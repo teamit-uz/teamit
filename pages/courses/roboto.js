1d5b3aa6a952cbc402f94416e5eaeb887c746709
Shahriyor <68349662+ShahriyorDeveloper@users.noreply.github.com>
2022-11-08 10:26:56 +0500

    price commented
    price commented

--- pages/courses/roboto.js
+++ pages/courses/roboto.js
@@ -80,9 +80,9 @@ const Course = () => {
             sertifikat beriladi.
           </h4>
         </div>
-        <div>
-          <span className={styles.bold}>Narxi: oyiga 250 ming so`m</span>
-        </div>
+//         <div>
+//           <span className={styles.bold}>Narxi: oyiga 250 ming so`m</span>
+//         </div>
       </div>
     </Layout>
   );
